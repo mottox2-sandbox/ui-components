50242418a624bb0ef03d51a8b52223dc366f2830
mottox2 <y.mottox2@gmail.com>
2021-08-04 17:17:27 +0900

    Add build scripts for tokens

---
+++ packages/tokens/src/build.js
@@ -0,0 +1,96 @@
+import path from "path";
+import theo from "theo";
+import fsExtra from "fs-extra";
+
+const { outputFile, readdir } = fsExtra;
+
+export const convert = async (file, format) => {
+  return theo
+    .convert({
+      transform: {
+        type: "web",
+        file,
+      },
+      format: {
+        type: format, // html, scss, module.js, common.js, custom-properties.css
+      },
+    })
+    .catch((error) => console.log(`Something went wrong: ${error}`));
+};
+
+(async () => {
+  theo.registerFormat(
+    "prefix.scss",
+    `{{#each props as |prop|}}
+  {{#if prop.comment~}}
+  {{{trimLeft (indent (comment (trim prop.comment)))}}}
+  {{/if~}}
+  \${{prop.type}}-{{kebabcase prop.name}}: {{#eq prop.type "string"}}"{{/eq}}{{{prop.value}}}{{#eq prop.type "string"}}"{{/eq}} !default;
+{{/each}}
+  `
+  );
+
+  const formats = ["prefix.scss", "common.js"];
+
+  const fileNames = (
+    await readdir(path.join(__dirname, "../tokens"), {
+      withFileTypes: true,
+    })
+  )
+    .filter((file) => file.isFile())
+    .map((file) => file.name)
+    .filter((fileName) => fileName.match(/yml$/));
+
+  await outputFile(
+    path.join(__dirname, "../dist/index.scss"),
+    `${fileNames
+      .map((fileName) => {
+        return `@import './${fileName.replace(".yml", "")}.scss';`;
+      })
+      .join("\n")}`
+  );
+
+  //   await outputFile(
+  //     path.join(__dirname, "../dist/index.js"),
+  //     `${fileNames
+  //       .map((fileName) => {
+  //         const name = fileName.replace(".yml", "");
+  //         return `const ${name} = require('./${name}');`;
+  //       })
+  //       .join("\n")}
+  // module.exports = {
+  // ${fileNames.map((fileName) => "  " + fileName.replace(".yml", "")).join(",\n")}
+  // }`
+  //   );
+
+  await outputFile(
+    path.join(__dirname, "../dist/index.js"),
+    `${fileNames
+      .map((fileName) => {
+        const name = fileName.replace(".yml", "");
+        return `import ${name} from './${name}';`;
+      })
+      .join("\n")}
+
+export default {
+${fileNames.map((fileName) => "  " + fileName.replace(".yml", "")).join(",\n")}
+}
+`
+  );
+
+  for (let i = 0; i < formats.length; i++) {
+    const format = formats[i];
+    for (let j = 0; j < fileNames.length; j++) {
+      const fileName = fileNames[j];
+      const result = await convert(
+        path.join(__dirname, `../tokens/${fileName}`),
+        format
+      );
+      const ext = format.split(".")[format.split(".").length - 1];
+      await outputFile(
+        path.join(__dirname, `../dist/${fileName.replace(".yml", "")}.${ext}`),
+        result
+      );
+    }
+  }
+})();
